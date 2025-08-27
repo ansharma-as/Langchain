427383cd89a0cd061012556e0cd25da3a508f45c
ansharma-as <anshsharma712.as@gmail.com>
2025-08-27 12:35:06 +0530

    added Similarity Searching

--- Document_Similaty.py
+++ Document_Similaty.py
@@ -1,5 +1,5 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from torch.nn.functional import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
 
 embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
 
@@ -11,14 +11,32 @@ docs = [
     "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
 ]
 
-query = "who is virat kohli"
+query = "who is Bumrah"
 
 doc_embeddings = embeddings.embed_documents(docs)
-
 query_embeddings = embeddings.embed_query(query)
 
 # print(doc_embeddings)
 # print(query_embeddings)
-similarities = cosine_similarity(query_embeddings.unsqueeze(0), doc_embeddings)
+# print(cosine_similarity([query_embeddings], doc_embeddings)[0])
 
-# print(cosine_similarity([query_embeddings], doc_embeddings))
+result = cosine_similarity([query_embeddings], doc_embeddings)[0]
+# print(sorted(list(enumerate(result)), key=lambda x: x[1])[-1])
+# by doing this we are giving index to each similarity score bcz they come randomly after cosine_similarity
+
+index, score = sorted(list(enumerate(result)), key=lambda x: x[1])[-1]
+
+print(query)
+print(docs[index])
+
+print("Similarity Score is: ", score)
+
+'''
+
+    what we are doing in this Document Similarity ?
+  
+    we are doing semantic search (similarity search)  based on scores in the docs array 
+    and finding which document is most similar to the query based on scores (This process is called retrieval Process)
+   
+   This is the basic RAG flow
+'''
